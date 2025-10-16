b63f4aff9aa3c2ccab74adeafcaac2b37bb06eca
Dava galang samudra <196321748+dava-alt-f4@users.noreply.github.com>
2025-10-16 11:20:42 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lamaran Kerja</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <header>
+        <nav class="flex justify-between py-3 px-5 bg-slate-200 items-center">
+            <div class="flex text-l items-center gap-10">
+                <h1 class="text-2xl font-semibold">Job Finder</h1>
+                <a href="/" class="hover:underline">Home</a>
+                <a href="/Jobs" class="hover:underline">Jobs</a>
+            </div>
+            <a href="/login" class="bg-blue-900 text-white py-1 px-2 rounded">Login</a>
+        </nav>
+    </header>
+</body>
+
+</html>
